673f3f6177b5a4aa7783def9fe857d220fa73c5c
haywhizzz <haywhizzz@gmail.com>
2020-11-24 16:02:13 +0100

    Style nav for homepage

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -52,4 +52,8 @@ private
     def category_params
         params.require(:category).permit(:name)
     end  
+
+    def set_category
+    @category = Category.find(params[:id])
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
     has_many :relationships, dependent: :destroy
     has_many :articles, through: :relationships 
-    
+    validates :name, presence: true, length: { in: 3..20 }
 
     
 end
