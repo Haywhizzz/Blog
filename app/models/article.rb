8bf2ca6ad51386e07041f201d7e9d60e7533a41f
haywhizzz <haywhizzz@gmail.com>
2020-11-26 12:04:32 +0100

    Styled article show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,10 +10,10 @@ class Article < ApplicationRecord
 
   has_many :categories, through: :relationships 
   
-  def list_of_categories
+  def category_list
     categories.collect(&:name).join(', ')
   end
-  def list_of_categories=(categories_string)
+  def category_list=(categories_string)
     category_names = categories_string.split(',').collect { |s| s.strip.downcase }.uniq
     new_or_found_categories = category_names.collect { |name| Category.find_or_create_by(name: name) }
     self.categories = new_or_found_categories
